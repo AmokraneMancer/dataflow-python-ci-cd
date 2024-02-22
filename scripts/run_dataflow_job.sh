fa374639d4a1ee49530ae4faa513cda9a5f1e778
Amokrane <moumouh.mancer@gmail.com>
2024-02-22 23:04:04 +0100

    edit sa

--- scripts/run_dataflow_job.sh
+++ scripts/run_dataflow_job.sh
@@ -12,8 +12,8 @@ gcloud dataflow flex-template run "$JOB_NAME-$CI_SERVICE_NAME-$(date +%Y%m%d-%H%
   --region="$LOCATION" \
   --temp-location="$TEMP_LOCATION" \
   --staging-location="$STAGING_LOCATION" \
+  --service-account-email="$SA_EMAIL" \
   --parameters project_id="$PROJECT_ID" \
-  --parameters service_account_email="$SA_EMAIL" \
   --parameters input_json_file="$INPUT_FILE" \
   --parameters team_league_dataset="$TEAM_LEAGUE_DATASET" \
   --parameters team_stats_table="$TEAM_STATS_TABLE"
